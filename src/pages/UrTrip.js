b448d4802ffdd3be92d6cb3507fd368b183fbd91
Logan Bondoc <loganbondoc@gmail.com>
2025-02-19 14:45:07 +1100

    began urtrip case study

--- src/pages/UrTrip.js
+++ src/pages/UrTrip.js
@@ -8,6 +8,7 @@ import CustomCarousel from '../components/CustomCarousel.js';
 import Carousel from 'react-bootstrap/Carousel';
 import CustomZoom from '../components/CustomZoom.js';
 import urTripBanner from '../images/urtrip/urtrip_banner.png';
+import {Divider} from "@heroui/divider";
 
 function UrTrip(){
     return(    
@@ -39,34 +40,27 @@ function UrTrip(){
                     <div class = "nav-line"></div>
                 </span>
             </section>
+
+            <Divider />
             
             <section class = "project-section">
-                <h2>The Problem</h2>
-                <h2>Low Engagement with Post-Booking Content</h2>
+                <h2 class="comfortaa">Low Engagement with Post-Booking Content</h2>
                 <h4>
-                Generic post-booking content and upselling of accommodations often feel impersonal and disconnected from the user’s specific itinerary, resulting in low engagement and missed revenue opportunities.
+                    Users find that generic post-booking content and upselling of accommodations often feels impersonal and disconnected from the user’s specific itinerary, resulting in low engagement and missed revenue opportunities.
                 </h4>
                 <h4>
-                Trip.com’s post-purchase page serves as one example of a common experience across booking platforms, airlines, and travel insurance providers. Similar layouts and content are found widely, highlighting a lack of tailored engagement in the industry.
+                    Trip.com’s post-purchase page serves as one example of a common experience across booking platforms, airlines, and travel insurance providers. Similar layouts and content are found widely, highlighting a lack of tailored engagement in the industry.
                 </h4>
-            </section>
-            <section class = "project-section">
-                <h2>Research Approach</h2>
-                <h4>Our design process for Memento, was based upon the double diamond method. Through this,  we implemented phases of:
-                    Discovering more about the chosen topic
-                    Defining the key issues that needed to be addressed
-                    Developing and ideating a key solution
-                    Delivering the solution to our target demographic</h4>
-            </section>
-            <section class = "project-section">
                 <h2>An Opportunity</h2>
-                <h4>Travel is supposed to be exciting! By offering personalised insights and tailored recommendations, these interactions could be transformed into valuable revenue streams that enhance the user experience.</h4>
+                <h4>
+                    Travel is supposed to be exciting! By offering personalised insights and tailored recommendations, these interactions could be transformed into valuable revenue streams that enhance the user experience.
+                </h4>
             </section>
             <section class = "project-section">
-                <h2>Introducing...</h2>
-            </section>
-            <section class = "project-section">
-                <h2>Concerns</h2>
+                <h2 class="comfortaa">Our Scope</h2>
+                <h4>
+                For our Minimum Viable Product (MVP), we focused on delivering a streamlined and impactful proof of concept that demonstrates the power of personalisation in the travel booking process. Instead of building a full-scale system, we prioritized essential elements that showcase core functionality while maintaining a seamless user experience.
+                </h4>
             </section>
         </div>
     );
