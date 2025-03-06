e37b8d9ecc7dd3f578f84ba757ae9c224f9df4f7
Logan Bondoc <loganbondoc@gmail.com>
2025-03-06 11:39:12 +1100

    began Memento redesign

--- src/pages/Memento.js
+++ src/pages/Memento.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Divider } from '@heroui/divider';
 
 // Images
 import mementoBanner from '../images/memento/memento_banner.png';
@@ -104,50 +105,63 @@ function Memento() {
             </section>
             <section class = "project-section">
                 <div>
-                    <h1>Memento</h1>
-                    <h3>Addressing Post-Party Effects in Young people</h3>
+                    <h3 class="header">Overview</h3>
+                    <h4>This investigation of </h4>
                 </div>
                 <div>
-                    <h2>Role</h2>
+                    <h3 class="header">Role</h3>
                     <h4>Product Designer: User Research, Interaction, Visual design, Prototyping & Testing</h4>
                 </div>
                 <span>
                     <div>
-                        <h2>Project Duration</h2>
+                        <h3 class="header">Project Duration</h3>
                         <h4>13 Weeks</h4>
                     </div>
                     <div>
-                        <h2>Sector</h2>
+                        <h3 class="header">Sector</h3>
                         <h4>Lifestyle, Leisure & Entertainment</h4>
                     </div>
-                    <div class = "nav-line"></div>
+                    <Divider />
                 </span>
             </section>
             
             <section class = "project-section">
-                <h2>The Problem Area</h2>
+            <h2 class="comfortaa">The Problem Area</h2>
                 <h4>
                 We've all experienced the excitement of attending gatherings, whether it's with friends, family, or even strangers. But what happens after the festivities? Consider this: in a study involving 28 brides, over half reported experiencing clinical levels of depression post-wedding.
 As psychoanalyst F. Diane Barth points out, there's often a surge of exhilaration during events, followed by a harsh reality check. The initial euphoria fades as the feel-good chemicals in our bodies diminish. Whether it's physical, emotional, or psychological, there's a notable pattern of post-event emotions emerging among younger generations.
                 </h4>
             </section>
+            <Divider />
+
             <section class = "project-section">
-                <h2>Research Approach</h2>
-                <h4>Our design process for Memento, was based upon the double diamond method. Through this,  we implemented phases of:
-                    Discovering more about the chosen topic
-                    Defining the key issues that needed to be addressed
-                    Developing and ideating a key solution
-                    Delivering the solution to our target demographic</h4>
-            </section>
-            <section class = "project-section">
+                <h2 class="comfortaa">Research Approach</h2>
+                <h4>
+                    Our design process for Memento, was based upon the double diamond method. Through this, we implemented phases of:<br/>
+                    <ul>
+                        <li>Discovering more about the chosen topic</li>
+                        <li>Defining the key issues that needed to be addressed</li>
+                        <li>Developing and ideating a key solution</li>
+                        <li>Delivering the solution to our target demographic</li>
+                    </ul>
+                    
+                </h4>
                 <div class = "centered-image-container">
                     <img class = "centered-image" src = {doubleDiamond}></img>
                 </div>
             </section>
+            <Divider />
 
             <section class = "project-section">
-                <h2>Discover</h2>
-                <h4>To begin our research, we conducted secondary research in the form of reviewing academic literature to see what debriefing methods were currently available and SWOT Analysis to see where these forms of recovery fall short. We found that:</h4>
+                <h3 class="header comfortaa">Secondary Research</h3>
+                <h4>To begin our research, we conducted secondary research in the form of reviewing academic literature to see what debriefing methods were currently available and SWOT Analysis to see where these forms of recovery fall short.<br/></h4>
+                <br/>
+                <h4>We found that:</h4>
+                <IconText
+                    img = {societalIcon}
+                    title = "Current recovery methods include:"
+                    description = "There is an expectation to sustain productivity, maintain appearances and update others on activity whilst experiencing post-event emotions.">
+                </IconText>
                 <div>
                     <h4>Academic Literature</h4>
                     <h4>Current recovery methods from Post Party Blues include: Jounalling thoughts and feelings Hosting Debriefing parties both online and in-person to facilitate reconnection</h4>
